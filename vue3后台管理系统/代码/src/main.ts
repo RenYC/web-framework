76fd422e35440f24db22f7f3333947f33c451c2d
renYC <renych@infogreat.cn>
2023-06-03 18:58:54 +0800

    feat: 【vue3-ts-admin】全局引入element-plus

--- vue3后台管理系统/代码/src/main.ts
+++ vue3后台管理系统/代码/src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import pinia from './store'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import 'normalize.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+app.use(ElementPlus)
 app.mount('#app')
