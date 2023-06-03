e584623ca91b31051f1cba1f81165f2c2448f581
renYC <renych@infogreat.cn>
2023-06-03 19:55:42 +0800

    feat: element-plus的图标引入和使用

--- vue3后台管理系统/代码/src/main.ts
+++ vue3后台管理系统/代码/src/main.ts
@@ -7,7 +7,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'normalize.css'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
 app.use(pinia)
 app.use(ElementPlus)
